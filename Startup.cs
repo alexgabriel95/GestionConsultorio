9bf2d20fe0a123a07c438b9ce7f242485b64a1ee
Alex Gabriel Rios Velazquez <alex.grvelazquez@hotmail.com>
2019-08-30 17:53:15 -0300

    #2 Se crea connection string en el start up

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GestionConsultorio.Models;
 
 namespace GestionConsultorio
 {
@@ -33,6 +35,10 @@ namespace GestionConsultorio
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = "server=localhost;user=root;database=gestionconsultorio_local;";
+            services.AddDbContext<DataContext>(options => options.UseMySql(connection));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
